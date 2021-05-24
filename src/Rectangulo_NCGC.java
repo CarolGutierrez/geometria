3cba946133d26e45c84a456731ee18cacfc4b57f
CarolGutierrez <neylin_gutierrez_@hotmail.com>
2021-05-24 16:20:37 +0200

    Versión comentada

--- src/Rectangulo_NCGC.java
+++ src/Rectangulo_NCGC.java
@@ -1,4 +1,9 @@
-
+/**
+ * 
+ * @author Neylin Carolina Gutiérrez Camargo
+ * Versión 1.2
+ * 24/05/2021
+ */
 public class Rectangulo_NCGC extends FiguraGeometrica_NCGC {
 	private double l1;
 	private double l2;
@@ -8,12 +13,16 @@ public class Rectangulo_NCGC extends FiguraGeometrica_NCGC {
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 * Método que calcula el area del rectangulo
+	 */	
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Metodo que calcula el perimetro del rectangulo
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
